9c0b3e931d70f61e1f7f753c9bdc1ba1da3a21a6
Nithin Kumar <78363835+Nithin-Kumar-N@users.noreply.github.com>
2024-03-30 19:18:34 +0530

    Time: 292 ms (92.31%), Space: 19.1 MB (99.41%) - LeetHub

---
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.py
@@ -0,0 +1,22 @@
+class Solution:
+    def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
+        def atMostK(nums, k):
+            count = [0] * (len(nums) + 1)
+            distinct = 0
+            left = 0
+            result = 0
+            for right in range(len(nums)):
+                if count[nums[right]] == 0:
+                    distinct += 1
+                count[nums[right]] += 1
+                
+                while distinct > k:
+                    count[nums[left]] -= 1
+                    if count[nums[left]] == 0:
+                        distinct -= 1
+                    left += 1
+                
+                result += right - left + 1
+            return result
+        
+        return atMostK(nums, k) - atMostK(nums, k - 1)
