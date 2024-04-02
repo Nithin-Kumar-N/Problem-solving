926cc7e58004debf1b5b9342b1d4e90afa61e01e
Nithin Kumar <78363835+Nithin-Kumar-N@users.noreply.github.com>
2024-04-02 10:21:06 +0530

    Time: 42 ms (62.18%), Space: 16.8 MB (32.93%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,19 +1,8 @@
-class Solution:
-    def isIsomorphic(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        
-        s_to_t = {}
-        
-        for char_s, char_t in zip(s, t):
-            if char_s in s_to_t:
-                if s_to_t[char_s] != char_t:
-                    return False
-            else:
-                if char_t in s_to_t.values():
-                    return False
-                s_to_t[char_s] = char_t
-        
-        return True
-
-        
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return len(set(zip(s,t))) == len(set(s)) == len(set(t))
