d252cfd07102376da6c3ef3dd4b38a8e9910f243
Nithin Kumar <78363835+Nithin-Kumar-N@users.noreply.github.com>
2024-04-07 18:32:29 +0530

    Time: 42 ms (19.08%), Space: 16.5 MB (91.06%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -0,0 +1,21 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        min_open = max_open = 0
+        
+        for char in s:
+            if char == '(':
+                min_open += 1
+                max_open += 1
+            elif char == ')':
+                if min_open > 0:
+                    min_open -= 1
+                max_open -= 1
+            else:  # char == '*'
+                if min_open > 0:
+                    min_open -= 1
+                max_open += 1
+            
+            if max_open < 0:  # more ')' than '(' and '*' combined
+                return False
+        
+        return min_open == 0
