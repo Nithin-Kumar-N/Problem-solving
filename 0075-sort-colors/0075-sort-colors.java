853421cce71f57c166b7028be7abb82c4b77615a
Nithin Kumar <78363835+Nithin-Kumar-N@users.noreply.github.com>
2024-06-13 21:25:04 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (60.52%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,26 +1,36 @@
-public class Solution {
+class Solution {
     public void sortColors(int[] nums) {
-        int red = 0;
-        int blue = nums.length - 1;
-        int curr = 0;
-        
-        while (curr <= blue) {
-            if (nums[curr] == 0) {
-                swap(nums, red, curr);
-                red++;
-                curr++;
-            } else if (nums[curr] == 2) {
-                swap(nums, curr, blue);
-                blue--;
+        int zeroCnt = 0, oneCnt = 0, twoCnt = 0;
+
+        // Count occurrences
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 0) {
+                zeroCnt++;
+            } else if(nums[i] == 1) {
+                oneCnt++;
             } else {
-                curr++;
+                twoCnt++;
             }
         }
-    }
-    
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+
+        int arrayIterator = 0;
+
+        // Place all 0s
+        for(int i = 0; i < zeroCnt; i++) {
+            nums[arrayIterator] = 0;
+            arrayIterator++;
+        }
+
+        // Place all 1s
+        for(int i = 0; i < oneCnt; i++) {
+            nums[arrayIterator] = 1;
+            arrayIterator++;
+        }
+
+        // Place all 2s
+        for(int i = 0; i < twoCnt; i++) {
+            nums[arrayIterator] = 2;
+            arrayIterator++;
+        }
     }
 }
